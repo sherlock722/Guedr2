44752a21ee153ff9a031b7d43a9c234340d294b6
Javier Cazorla <sherlock722@gmail.com>
2016-04-12 00:06:42 +0200

    Añadimos el Menu

--- app/src/main/java/com/fjc/guedr2/ForecastActivity.java
+++ app/src/main/java/com/fjc/guedr2/ForecastActivity.java
@@ -1,7 +1,10 @@
 package com.fjc.guedr2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.TextureView;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,6 +16,7 @@ public class ForecastActivity extends AppCompatActivity {
     private TextView mHumidity;
     private TextView mDescription;
     private ImageView mForecast_image;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +31,49 @@ public class ForecastActivity extends AppCompatActivity {
          mForecast_image = (ImageView) findViewById(R.id.forecast_img);
 
 
+
         //Creo el modelo
         Forecast forecast = new Forecast(20,10,25,"Sol y nubes", R.drawable.sun_cloud);
-         mMax_temp.setText(String.valueOf(forecast.getMaxTemp()));
+         //mMax_temp.setText(String.valueOf(forecast.getMaxTemp()));
+        mMax_temp.setText(String.format("Temperatura máxima: %.2f", forecast.getMaxTemp()));
+        mMin_temp.setText(String.format("Temperatura mínima: %.2f", forecast.getMinTemp()));
+        mHumidity.setText(String.format("Humedad: %.2f", forecast.getHumidity()));
+        mDescription.setText(forecast.getDescription());
+        //Para el icono utilizamos un método que nos devuelve un recurso
+        mForecast_image.setImageResource(forecast.getIcon());
 
 
+    }
+
+    //Indica como es el Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        //Llamo al método del padre
+        super.onCreateOptionsMenu(menu);
+        //Para el Menu
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        //return super.onCreateOptionsMenu(menu);
+        return true; //Se han realizado modificaciones
+    }
+
+    //Indica que pasa al pulsar una opción de Menú
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        boolean superReturn = super.onOptionsItemSelected(item);
+
+        if (item.getItemId()==R.id.menu_show_settings) {
+
+            //Lanzamos la actividad SettingsActivity utilizando un intent explicito
+            //ya que decimos que actividad explicitamente se tiene que lanzar
+            Intent intent = new Intent(this,SettingActivity.class);
+
+            //Lanzamos el intent explicito para lanzar la pantalla de ajustes
+            startActivity(intent);
+            return true;
+        }
+
+        return superReturn;
     }
 }
---
+++ app/src/main/java/com/fjc/guedr2/SettingActivity.java
@@ -0,0 +1,53 @@
+package com.fjc.guedr2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioGroup;
+
+/**
+ * Created by javier on 11/4/16.
+ */
+public class SettingActivity extends AppCompatActivity {
+
+    private RadioGroup mRadioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        // Accedo a las vistas
+        mRadioGroup = (RadioGroup) findViewById(R.id.units_rg);
+
+        findViewById(R.id.accept_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                acceptSettings();
+            }
+        });
+
+        findViewById(R.id.cancel_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelSettings();
+            }
+        });
+
+    }
+
+    private void cancelSettings() {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
+    private void acceptSettings() {
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("units", mRadioGroup.getCheckedRadioButtonId());
+
+        setResult(RESULT_OK, returnIntent);
+
+        finish();
+    }
+}
